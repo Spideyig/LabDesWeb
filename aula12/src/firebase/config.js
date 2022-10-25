f8b79199f1f09d2092895b32fa7b64de8292ce3b
Spiderig <igor01mj@gmail.com>
2022-10-25 10:20:47 -0300

    firebase_configuração

---
+++ aula12/src/firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDBptjlU_aCm_MlD_E-CEdh1-ZZ2B8MAZE",
+  authDomain: "labdesweb-96ab4.firebaseapp.com",
+  projectId: "labdesweb-96ab4",
+  storageBucket: "labdesweb-96ab4.appspot.com",
+  messagingSenderId: "559271722322",
+  appId: "1:559271722322:web:cb30394256ad2c584d4e8b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
